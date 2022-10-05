09f5d12c97aad97a80c5e09451903d9cb9cd7bca
guihi1 <gui.hikio@gmail.com>
2022-10-05 15:17:52 -0300

    Add event listener to change button text

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import weatherInterface from './weatherInterface';
 import getWeather from './getWeather';
 
 const search = document.getElementById('search-city');
+const changeUnit = document.getElementById('degree');
 
 getWeather('london').then((data) => weatherInterface(data));
 
@@ -10,4 +11,13 @@ search.addEventListener('keyup', (e) => {
   if (e.key === 'Enter') {
     getWeather(document.getElementById('search-city').value).then(((data) => weatherInterface(data)));
   }
+});
+
+changeUnit.addEventListener('click', () => {
+  if (changeUnit.textContent === '°C') {
+    changeUnit.textContent = '°F';
+  } else {
+    changeUnit.textContent = '°C';
+  }
+  getWeather(document.getElementById('city-name').textContent).then(((data) => weatherInterface(data)));
 });
