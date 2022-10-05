2cc81250fd6c52dc647f9bb6bc23fc4bd73b2dbf
guihi1 <gui.hikio@gmail.com>
2022-10-05 18:20:22 -0300

    Change background according to weather

--- src/weatherInterface.js
+++ src/weatherInterface.js
@@ -1,3 +1,30 @@
+import cloudy from './images/cloudy.jpg';
+import normal from './images/default.jpg';
+import rain from './images/rain.jpg';
+import sunny from './images/sunny.jpg';
+import thunder from './images/thunderstorm.jpg';
+import snow from './images/snow.jpg';
+import drizzle from './images/drizzle.jpg';
+
+function changeBackground(weather) {
+  const body = document.querySelector('body');
+  if (weather === 'Clouds') {
+    body.style.background = `url(${cloudy})`;
+  } else if (weather === 'Rain') {
+    body.style.background = `url(${rain})`;
+  } else if (weather === 'Clear') {
+    body.style.background = `url(${sunny})`;
+  } else if (weather === 'Thunderstorm') {
+    body.style.background = `url(${thunder})`;
+  } else if (weather === 'Drizzle') {
+    body.style.background = `url(${drizzle})`;
+  } else if (weather === 'Snow') {
+    body.style.background = `url(${snow})`;
+  } else {
+    body.style.background = `url(${normal})`;
+  }
+}
+
 export default function weatherInterface(obj) {
   const weather = document.querySelector('.weather');
   const description = document.querySelector('.description');
@@ -5,11 +32,22 @@ export default function weatherInterface(obj) {
   const feelsLike = document.querySelector('.feels-like');
   const humidity = document.querySelector('.humidity');
   const city = document.getElementById('city-name');
-
-  city.textContent = `${obj.name}, ${obj.sys.country}`;
-  weather.textContent = obj.weather[0].main;
-  description.textContent = obj.weather[0].description;
-  temperature.textContent = `Temperature: ${obj.main.temp}`;
-  feelsLike.textContent = `Feels like: ${obj.main.feels_like}`;
-  humidity.textContent = `Humidity: ${obj.main.humidity}`;
+  const weatherImg = document.getElementById('weather-icon');
+  if (obj.hasOwnProperty('message') && obj.hasOwnProperty('cod')) {
+    city.textContent = obj.message;
+    weather.textContent = '';
+    description.textContent = '';
+    temperature.textContent = '';
+    feelsLike.textContent = '';
+    humidity.textContent = '';
+  } else {
+    city.textContent = `${obj.name}, ${obj.sys.country}`;
+    weather.textContent = obj.weather[0].main;
+    description.textContent = obj.weather[0].description;
+    temperature.textContent = `Temperature: ${obj.main.temp}`;
+    feelsLike.textContent = `Feels like: ${obj.main.feels_like}`;
+    humidity.textContent = `Humidity: ${obj.main.humidity}%`;
+    weatherImg.src = `http://openweathermap.org/img/wn/${obj.weather[0].icon}@2x.png`;
+    changeBackground(obj.weather[0].main);
+  }
 }
