a037e4a9209f82f4f1f97f20c5ccac69760b07f6
guihi1 <gui.hikio@gmail.com>
2022-10-05 14:32:59 -0300

    Add event listener to search button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './styles.css';
+import weatherInterface from './weatherInterface';
+import getWeather from './getWeather';
+
+const search = document.getElementById('search-city');
+
+getWeather('london').then((data) => weatherInterface(data));
+
+search.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') {
+    getWeather(document.getElementById('search-city').value).then(((data) => weatherInterface(data)));
+  }
+});
