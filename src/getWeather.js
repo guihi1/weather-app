535615112cc7cbe4d47e74c5779899b4568063df
guihi1 <gui.hikio@gmail.com>
2022-10-05 16:44:27 -0300

    Make it possible to change between °C and °F

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,9 +1,16 @@
 export default async function getWeather(city) {
   try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=74cbb669a29e0705f039a0efb0eefe7e&units=metric`, { mode: 'cors' });
-    const data = response.json();
+    let data;
+    if (document.querySelector('#degree').textContent === '°C') {
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=74cbb669a29e0705f039a0efb0eefe7e&units=metric`, { mode: 'cors' });
+      data = response.json();
+    } else {
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=74cbb669a29e0705f039a0efb0eefe7e&units=imperial`, { mode: 'cors' });
+      data = response.json();
+    }
     return data;
   } catch (err) {
-    console.log(err);
+    const cityName = document.getElementById('city-name');
+    cityName.textContent = err;
   }
 }
